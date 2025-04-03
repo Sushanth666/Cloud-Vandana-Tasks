ccc62ebbebc32c2d6b11116b3137718d56c0ab34
B V Sree Sushanth <bvsreesushanth29802@gmail.com>
2025-04-03 11:57:11 +0530

    Task2.java

---
+++ Task2.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class AnagramChecker {
+    public static boolean isAnagram(String str1, String str2) {
+        char[] arr1 = str1.replaceAll("\\s", "").toLowerCase().toCharArray();
+        char[] arr2 = str2.replaceAll("\\s", "").toLowerCase().toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+        System.out.println("Are they anagrams? " + isAnagram(str1, str2));
+    }
+}
